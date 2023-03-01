2fe6483ac60c6a47a0a89857dac96a92b2003537
Alex Andras Mandrik <alexmandrik@pm.me>
2023-03-01 11:30:18 +0100

    Enable autocompletion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,17 @@
 oh-my-posh init pwsh | Invoke-Expression
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
+
+function Install-ModulesForWork(){
+    # For autocompletion
+    Install-Module PSReadLine;
+}
